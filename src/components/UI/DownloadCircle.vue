4087daf50d6dc246fd93fb3fd3e7c7e9a57be1fc
Vlad <cherkovvlad@gmail.com>
2023-03-10 15:32:39 +0300

    v1.1

--- src/components/UI/DownloadCircle.vue
+++ src/components/UI/DownloadCircle.vue
@@ -12,7 +12,9 @@ export default {
 <style scoped>
 .download{
   border-bottom: 10px solid black;
-  border-radius: 40px;
-  width: 200px;
+  border-radius: 50%;
+  width: 400px;
+  height: 400px;
+  position: absolute;
 }
 </style>
